f106af3758225d818ed44a1a0a21dd8ecbb5892c
Ysfcelikkaya <99491427+Ysfcelikkaya@users.noreply.github.com>
2023-12-13 22:38:32 +0300

    Compenent3

---
+++ quasar-project/src/components/TheSecan.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <h5><strong>Öne Çıkanlar</strong></h5>
+
+      <div class="q-pa-md q-gutter-md">
+        <div class="row justify-between">
+  
+          <q-parallax src="src\assets\images\heat-desktop.webp"></q-parallax>
+  
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  
