9205f9f6b6c200e97174b88466e28d4d7c7c3b73
Ysfcelikkaya <99491427+Ysfcelikkaya@users.noreply.github.com>
2023-12-13 22:38:54 +0300

    Component5

---
+++ quasar-project/src/components/TheSecande.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <h5><strong>Aktiviteni Seç</strong></h5>
+  
+      <div class="q-pa-md">
+        <div class="q-col-gutter-md row items-start">
+
+          <div class="col-3">
+            <q-img src="src\assets\images\Casual-Desktop.webp" style="height: 350px;"></q-img>
+          </div>
+  
+          <div class="col-3">
+            <q-img src="src\assets\images\hiking_desktop.webp" style="height: 350px;"></q-img>
+          </div>
+
+          <div class="col-3">
+            <q-img src="src\assets\images\Doga-desktop.webp" style="height: 350px;"></q-img>
+          </div>
+
+          <div class="col-3">
+            <q-img src="src\assets\images\Kayak-Desktop.webp" style="height: 350px;"></q-img>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+
+  <script>
+  export default {
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  
