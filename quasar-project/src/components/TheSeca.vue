8cebe71ba92cdfccc05dead23fc3536f47714c89
Ysfcelikkaya <99491427+Ysfcelikkaya@users.noreply.github.com>
2023-12-13 22:38:20 +0300

    Compenent2

---
+++ quasar-project/src/components/TheSeca.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-col-gutter-md row items-start">
+      <div class="col-4" style="text-align: center;">
+        <q-img
+          src="src\assets\images\Man-desktop.webp"
+          :ratio="1"
+        />
+        <strong>Erkek</strong>
+      </div>
+
+      <div class="col-4" style="text-align: center;">
+        <q-img
+          src="src\assets\images\Women-desktop.webp"
+          :ratio="1"
+        />
+        <strong>Kadın</strong>
+      </div>
+
+      <div class="col-4" style="text-align: center;">
+        <q-img
+          src="src\assets\images\Kids-desktop.webp"
+          :ratio="1"
+        />
+        <strong>Çocuk</strong>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style scoped>
+</style>
